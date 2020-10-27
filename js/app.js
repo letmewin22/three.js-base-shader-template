823fbf5345d071db16093c119f00edbfd1278ae5
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2020-10-27 18:18:48 +0200

    create material and animations

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
 import * as THREE from 'three'
 import * as dat from 'dat.gui'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import vertex from './shader/vertex.glsl'
-import fragment from './shader/fragment.glsl'
 
 
+const clamp = (num, a, b) => {
+  return Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b))
+}
 
 class Sketch {
   constructor($selector, $imgs = []) {
-
     this.$container = document.querySelector($selector)
     this.$imgs = $imgs
     this.time = 0
+    this.result = 0
 
     this.sizes = {
       w: window.innerWidth,
-      h: window.innerHeight
+      h: window.innerHeight,
     }
 
     this.mouse = {
       destX: 0,
-      destY: 0
+      destY: 0,
     }
 
     this.resize = this.resize.bind(this)
@@ -34,14 +35,13 @@ class Sketch {
   }
 
   init() {
-
     this.scene = new THREE.Scene()
 
     this.camera = new THREE.PerspectiveCamera(
       70,
       this.sizes.w / this.sizes.h,
       0.01,
-      1000,
+      1000
     )
     this.camera.position.z = 1
 
@@ -57,16 +57,22 @@ class Sketch {
     this.renderer.setClearColor(0xd3d3d3, 0)
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.geometry1 = new THREE.TorusGeometry( 0.25, 0.035, 10, 100 )
-    this.geometry2 = new THREE.TorusGeometry( 0.25, 0.035, 10, 100 )
+    this.geometry1 = new THREE.TorusGeometry(0.3, 0.045, 100, 100)
+    this.geometry2 = new THREE.TorusGeometry(0.3, 0.045, 100, 100)
 
-    this.material = new THREE.ShaderMaterial({
-      uniforms: {
-        uTexture: {type: 't', value: 0}
-      },
-      vertexShader: vertex,
-      fragmentShader: fragment,
-      transparent: true
+    // this.light = new THREE.AmbientLight( 0x404040 )
+    this.light = new THREE.DirectionalLight('#ffe6ae', 5)
+    this.light.position.set(7.5, 5, 10)
+
+    this.scene.add( this.light )
+
+    this.material = new THREE.MeshStandardMaterial({
+      color: 0x30cb2,
+      metalness: 0.87,
+      flatShading: false,
+      roughness: 0.2,
+      depthTest: true,
+      depthWrite: true
     })
 
     this.mesh1 = new THREE.Mesh(this.geometry1, this.material)
@@ -74,9 +80,7 @@ class Sketch {
 
     this.mesh2.position.x = this.settings.position
     this.mesh2.rotation.x = this.settings.rotationX
-    this.mesh2.rotation.y = this.settings.rotationY
     this.scene.position.x = this.settings.scenePos
-    
 
     this.scene.add(this.mesh1)
     this.scene.add(this.mesh2)
@@ -95,49 +99,91 @@ class Sketch {
   }
 
   datGui() {
-
     this.settings = {
-      position: 0.45,
+      position: 0.51,
       rotationX: 1.07,
-      rotationY: 1.78,
       scenePos: -0.225,
-      sceneZ: 0.5
+      sceneZ: 0.5,
+      color: 3036343,
+      roughness: 0.24,
+      metalness: 0.77,
+      flatShading: false,
+      radius: 0.3, 
+      tube: 0.045, 
+      radialSegments: 100, 
+      tubularSegments: 200
     }
 
     this.gui = new dat.GUI()
 
-    this.gui.add(this.settings, 'position', 0, 1, 0.01).onChange(() => this.mesh2.position.x = this.settings.position)
-    this.gui.add(this.settings, 'rotationX', 0, 5, 0.01).onChange(() => this.mesh2.rotation.x = this.settings.rotationX)
-    this.gui.add(this.settings, 'rotationY', 0, 5, 0.01).onChange(() => this.mesh2.rotation.y = this.settings.rotationY)
-    this.gui.add(this.settings, 'scenePos', -5, 5, 0.01).onChange(() => this.scene.position.x = this.settings.scenePos)
+    this.gui
+      .add(this.settings, 'position', 0, 1, 0.01)
+      .onChange(() => (this.mesh2.position.x = this.settings.position))
+    this.gui
+      .add(this.settings, 'rotationX', 0, 5, 0.01)
+      .onChange(() => (this.mesh2.rotation.x = this.settings.rotationX))
+    this.gui
+      .add(this.settings, 'scenePos', -5, 5, 0.01)
+      .onChange(() => (this.scene.position.x = this.settings.scenePos))
+    this.gui
+      .addColor(this.settings, 'color')
+      .onChange(() => {
+        this.material.color = new THREE.Color(this.settings.color)
+        this.material.needsUpdate = true
+      }
+      )
+    this.gui
+      .add(this.settings, 'roughness', 0, 1, 0.01)
+      .onChange(() => (this.material.roughness = this.settings.roughness))
+    this.gui
+      .add(this.settings, 'metalness', 0, 1, 0.01)
+      .onChange(() => (this.material.metalness = this.settings.metalness))
+    this.gui
+      .add(this.settings, 'flatShading')
+      .onChange(() => {
+        this.material.flatShading = this.settings.flatShading 
+        this.material.needsUpdate = true
+      })
+    this.gui
+      .add(this.settings, 'radius', 0, 1, 0.01)
+      .onChange(() => {
+        this.mesh1.geometry.parameters.radius = this.settings.radius
+        this.mesh2.geometry.parameters.radius = this.settings.radius
+      })
+    this.gui
+      .add(this.settings, 'tube', 0, 1, 0.01)
+      .onChange(() => {
+        this.mesh1.geometry.parameters.tube = this.settings.tube
+        this.mesh2.geometry.parameters.tube = this.settings.tube
+      })
   }
 
+
   animate() {
-    this.animateRAF = () => {
-      requestAnimationFrame(this.animateRAF)
+      requestAnimationFrame(this.animate.bind(this))
       this.time++
-      this.scene.rotation.x += (this.mouse.destX - this.scene.rotation.x)*0.05;
-      this.scene.rotation.y += (this.mouse.destY - this.scene.rotation.y)*0.05;
+      this.scene.rotation.x += (this.mouse.destY - this.scene.rotation.x) * 0.025
+      this.scene.rotation.y += (this.mouse.destX - this.scene.rotation.y) * 0.025
+
+      this.result += (this.mouse.destX - this.scene.rotation.x) * 0.001
+
+      this.result = clamp(this.result, 0.45, 0.48)
+
+      this.mesh2.position.x = this.result
+      this.mesh2.scale.x = 1 + ((this.mouse.destX - this.mesh2.scale.x) * 0.025)
+      this.mesh2.scale.y = 1 + ((this.mouse.destX - this.mesh2.scale.y) * 0.025)
+      this.mesh1.scale.x = 1 + ((this.mouse.destX - this.mesh1.scale.x) * 0.04)
+      this.mesh1.scale.y = 1 + ((this.mouse.destX - this.mesh1.scale.y) * 0.04)
+
       this.renderer.render(this.scene, this.camera)
-    }
-    this.animateRAF()
   }
 
   onMousemove(e) {
-    const x = (e.clientX-this.sizes.w/2)/(this.sizes.w/2)
-    const y = (e.clientY-this.sizes.h/2)/(this.sizes.h/2)
-    this.mouse.destX = y*0.5
-    this.mouse.destY = x*0.5
+    const x = (e.clientX - this.sizes.w / 2) / (this.sizes.w / 2)
+    const y = (e.clientY - this.sizes.h / 2) / (this.sizes.h / 2)
+    this.mouse.destX = -x * 0.5
+    this.mouse.destY = -y * 0.5
   }
 }
 
 new Sketch('#gl')
-
-
-
-// function render() {
-//   scene.rotation.x += (scene.destination.x - scene.rotation.x)*0.05;
-//   scene.rotation.y += (scene.destination.y - scene.rotation.y)*0.05;
-//   renderer.render(scene, camera);
-// }
-
